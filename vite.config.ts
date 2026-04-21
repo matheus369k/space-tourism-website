36bb4a31db0bc93d08089384ea7a25f68a1e2390
matheus369k <gomes.craft10@gmail.com>
2026-04-21 16:51:47 -0300

    build: disabled sourcemap in production

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ dotenv.config({ quiet: true })
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  build: {
+    sourcemap: process.env.NODE_ENV !== 'production',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
