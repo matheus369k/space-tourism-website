2f7ac62ef157e889b1691c76ebd99912cca51d4d
matheus369k <gomes.craft10@gmail.com>
2026-04-21 13:22:48 -0300

    feat: add google analyts

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app.tsx'
+import ReactGA from 'react-ga4'
+import { env } from './util/env'
 import './index.css'
+
+if (env.VITE_GA_ID) {
+  ReactGA.initialize(env.VITE_GA_ID)
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/util/env.ts
+++ src/util/env.ts
@@ -1,5 +1,8 @@
 import z from 'zod'
 
-const schemaEnv = z.object({})
+const googleAnalystRegex = /^G-[A-Z0-9]{10}$/
+const schemaEnv = z.object({
+  VITE_GA_ID: z.string().regex(googleAnalystRegex).optional(),
+})
 
 export const env = schemaEnv.parse(import.meta.env)
